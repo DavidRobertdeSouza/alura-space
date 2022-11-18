eff0392239ddaf163f2929b51f2acb72af33bf26
David Robert de Souza <david_robert97@outlook.com>
2022-11-18 11:19:47 -0300

    Criado rodape e adicionado variaveis com fontes

--- src/componentes/Banner/index.jsx
+++ src/componentes/Banner/index.jsx
@@ -5,7 +5,7 @@ import styles from './Banner.module.scss'
 export default function Banner() {
   return (
     <div className={styles.imagem}>
-      <h1>A galeria mais completa do espeço</h1>
+      <h1>A galeria mais completa do espaço!</h1>
       <img src={banner} alt="A imagem da terra vista do espaço" />
     </div>
   )
